e7cbb36056cec96e4644c8e9cf9cfb347c77ce27
huxiaoyue <huxiaoyue@daqinjia.cn>
2020-04-01 11:39:53 +0800

    删除虚拟环境

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/huxiaoyue/Desktop/python_work/django_project/12/demo12a/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
